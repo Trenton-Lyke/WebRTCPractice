6d1a4b0afb3a89b5eeff2e1c050f3bd2ae41c4a9
Trenton Lyke <tjl223@cornell.edu>
2021-03-23 16:17:18 -0400

    First commit

---
+++ backend/dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+io.on('connection', function (socket) {
+    socket.emit('me', socket.id);
+    socket.on('disconnect', function () {
+        socket.broadcast.emit('callEnded');
+    });
+    socket.on('callUser', function (data) {
+        io.to(data.userToCall).emit('callUser', { signal: data.signalData, from: data.from, name: data.name });
+    });
+    socket.on('answerCall', function (data) { return io.to(data.to).emit('callAccepted', data.signal); });
+});
+server.listen(5000, function () { return console.log('server listening on port 5000'); });
+//# sourceMappingURL=index.js.map
